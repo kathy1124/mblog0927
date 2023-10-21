ceb2741f61f0c5a79c0c0ae45418b9a25b1a1f58
kathy1124 <110560018@ntub.edu.tw>
2023-10-21 18:55:09 +0800

    註解

--- mysite/admin.py
+++ mysite/admin.py
@@ -3,5 +3,5 @@ from mysite.models import Post   #定義的post納入管理
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'pub_date')
+    list_display = ('title', 'slug', 'pub_date')  #文章顯示時有 title、slug、pub_date(標題、網址、日期)
 admin.site.register(Post, PostAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200) #標題
-    slug = models.CharField(max_length=200)  #標題
+    slug = models.CharField(max_length=200)  #連結
     body = models.TextField()                #長文字(內文)
-    pub_date = models.DateTimeField(auto_now_add=True) #時間
-     
+    pub_date = models.DateTimeField(auto_now_add=True) #本文發表的時間
+                                    #資料庫在建立時自動加入當時的時間
     class Meta:      #'-'反向排序
-         ordering = ('-pub_date', ) #排序大到小
-     
+        ordering = ('-pub_date', ) #排序大到小
+    
     def __str__(self) -> str:  #python內建
-        return self.title
+        return self.title   #產生項目資料時以文章標題的內容為代表
