ff58af060b3e9f28bf2b5e71b5d79be4c4171eda
kathy1124 <11056018@ntub.edu.tw>
2023-12-15 23:53:35 +0800

    留言板

--- mytest/views.py
+++ mytest/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from mytest.models import Post, Mood
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 # Create your views here.
 def index(request):
-    post = Post.objects.filter(enabled=True).order_by('pub_time')[:30]
+    posts = Post.objects.filter(enabled=True).order_by('-pub_time')[:30] #用pub_time倒序 顯示前3筆
     moods= Mood.objects.all()
     if request.method == 'GET':
         return render(request, 'myform.html', locals())
@@ -12,16 +11,16 @@ def index(request):
             user_id = request.POST['user_id']
             user_pass = request.POST['user_pass']
             user_post = request.POST['user_post']
-            user_mood = request.POST['user_mood']
+            user_mood = request.POST['mood']
             mood = Mood.objects.get(status=user_mood)
-            post = Post(mood=mood, nickname=user_id, del_pass=user_post )
+            post = Post(mood=mood, nickname=user_id, del_pass=user_post, message=user_post )
             post.save()
-            message = '成功儲存！請儲存你的編輯密碼[{}]!''訊息需審查後才會顯示',format(user_pass)
-            return redirect("test-new",locals())
+            message = f'成功儲存！請儲存你的編輯密碼[{user_pass}]!，訊息需審查後才會顯示'.format(user_pass)
+            return render(request,"myform.html",locals())
         except Exception as e:
             print(e)
             message = '出現錯誤'
             return render(request, 'myform.html', locals())
     else:
-        message = '/post/get 出現錯誤'
+        message ='/post/get 出現錯誤'
         return render(request, 'myform.html', locals())
