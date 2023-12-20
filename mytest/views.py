7b57131595d75774abbc3274bd6dbee631c699a6
kathy1124 <11056018@ntub.edu.tw>
2023-12-20 14:10:54 +0800

    show 心情

--- mytest/views.py
+++ mytest/views.py
@@ -3,7 +3,7 @@ from mytest.models import Post, Mood
 from django.shortcuts import redirect
 
 def index(request): 
-    post = Post.objects.filter(enabled=True).order_by('-pub_time')[:30]#[:30]取前三十個,日期排序
+    posts = Post.objects.filter(enabled=True).order_by('-pub_time')[:30]#[:30]取前三十個,日期排序
     moods = Mood.objects.all()#把東西從資料庫抓出來
     if request.method == 'GET':
         return render(request,'myform.html',locals())
@@ -14,7 +14,7 @@ def index(request):
             user_post = request.POST['user_post']
             user_mood = request.POST['mood']
             mood = Mood.objects.get(status=user_mood)
-            post = Post(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)#先把物件綁定到post裡面
+            post = Post(moods=mood, nickname=user_id, del_pass=user_pass, message=user_post)#先把物件綁定到post裡面
             post.save()
             message = '成功儲存！請記得你的編輯密碼[{}]!，訊息需經審查後才會顯示。'.format(user_pass)
             return render(request,'myform.html',locals())
